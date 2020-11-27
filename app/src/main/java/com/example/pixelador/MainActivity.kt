a01eefcd39d139ca387d8364471c3cb90058c7a8
Samuel <ssanjuanandres@danielcastelao.org>
2020-11-27 14:41:54 +0100

    no se

--- app/src/main/java/com/example/pixelador/MainActivity.kt
+++ app/src/main/java/com/example/pixelador/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.pixelador
 
 import android.graphics.BitmapFactory
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -17,16 +18,13 @@ class MainActivity : AppCompatActivity() {
         val pixelar:Button=findViewById(R.id.pixelar)
         val normal:Button=findViewById(R.id.normal)
         val aumentar:Button=findViewById(R.id.aumentar)
+        val prueba:Button=findViewById(R.id.pruebas)
 
         val options =BitmapFactory.Options()
         options.inSampleSize=1
         options.inMutable=true
 
         var imagenBitmap=BitmapFactory.decodeResource(resources,R.drawable.zenitsu,options)
-
-        val ancho=imagenBitmap.width
-        val alto=imagenBitmap.height
-
 
 
         minimizar.setOnClickListener(){
@@ -56,22 +54,57 @@ class MainActivity : AppCompatActivity() {
         var altoY:Int
 
         pixelar.setOnClickListener(){
-            for (x in 0..ancho/16-1){
-                for (y in 0..alto/16-1){
+            for (x in 0..imagenBitmap.width/32-1){
+                for (y in 0..imagenBitmap.height/32-1){
 
-                    anchoX=x*16
-                    altoY=y*16
+                    anchoX=x*32
+                    altoY=y*32
                     var c=imagenBitmap.getPixel(anchoX, altoY)
 
-                    var colores:IntArray=intArrayOf(c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c)
+                    var colores:IntArray=intArrayOf(c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,
+                            c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,
+                            c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,
+                            c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,
+                            c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,
+                            c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,
+                            c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,
+                            c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,
+                            c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,
+                            c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,
+                            c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,
+                            c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,
+                            c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c,c)
 
-                            if (anchoX+16<ancho&&altoY+16<alto) {
-                        imagenBitmap.setPixels(colores,0,16,anchoX,altoY,16,16)
-                    }
+                        imagenBitmap.setPixels(colores,0,32,anchoX,altoY,32,32)
                 }
             }
 
             imagen.setImageBitmap(imagenBitmap.scale(1200, 600))
+        }
+
+        prueba.setOnClickListener(){
+
+            var colorFinal=0
+            var colores=ArrayList<Int>()
+            var coloresArray :IntArray
+            for (x in 0..imagenBitmap.width/32-1){
+                for (y in 0..imagenBitmap.height/32-1){
+                    for (x0 in 0..x){
+                        for (y0 in 0..y){
+                            var colorInt=imagenBitmap.getPixel(x0,y0)
+                            colorFinal+=colorInt
+                        }
+                    }
+                    colorFinal=colorFinal/(32*32)
+
+                    colores.add(colorFinal)
+
+
+                }
+            }
+             coloresArray=colores.toIntArray()
+            println(colores.toString())
+            imagenBitmap.setPixels(coloresArray,0,32,0,0,32,32)
         }
     }
 }
